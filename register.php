981fe258c3cb6480d6bd78c5e8e3e3e265406d53
Rizky Alfito <ikycollege@gmail.com>
2025-08-10 08:29:37 +0700

    finish block register

--- register.php
+++ register.php
@@ -16,7 +16,7 @@ session_start();
     $sql = mysqli_query($koneksi, "SELECT * FROM identitas");
     $row1 = mysqli_fetch_assoc($sql);
     ?>
-    <title>Pendaftaran | <?= $row1['nama_app']; ?></title>
+    <title>Akses Ditolak | <?= $row1['nama_app']; ?></title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
@@ -27,8 +27,6 @@ session_start();
     <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="assets/plugins/iCheck/square/blue.css">
     <!-- Icon -->
     <link rel="icon" type="icon" href="assets/dist/img/logo_app.png">
     <!-- Custom -->
@@ -45,36 +43,18 @@ session_start();
         <!-- /.login-logo -->
         <div class="login-box-body" style="border-radius: 10px;">
             <img src="assets/dist/img/logo_app.png" height="80px" width="80px" style="display: block; margin-left: auto; margin-right: auto; margin-top: -12px; margin-bottom: 5px;">
-
-            <form name="formLogin" action="function/Process.php?aksi=daftar" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="funame" placeholder="Nama Lengkap" id="funame">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="uname" placeholder="Nama Pengguna" id="uname">
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="passw" placeholder="Kata Sandi" id="passw">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <!-- /.col -->
-                    <div class="col-xs-12">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Daftar Sekarang</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
+            
+            <div class="text-center" style="margin-bottom: 20px;">
+                <i class="fa fa-lock fa-3x text-red" style="margin-bottom: 10px;"></i>
+                <h3 style="font-family: 'Quicksand', sans-serif; font-weight: bold;">Akses Registrasi Ditutup</h3>
+                <p style="font-size: 14px; color: #666;">
+                    Registrasi akun baru hanya dapat dilakukan oleh admin melalui halaman admin.<br>
+                    Silakan hubungi admin untuk membuat akun baru.
+                </p>
+            </div>
 
             <div class="social-auth-links text-center">
-                <p style="font-size: 11px;">- ATAU -</p>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <button type="button" onclick="Masuk()" class="btn btn-block btn-success btn-flat"><i class="fa fa-sign-in"></i> Sudah mempunyai akun ? Masuk</button>
-                    </div>
-                </div>
+                <button type="button" onclick="Masuk()" class="btn btn-block btn-success btn-flat"><i class="fa fa-sign-in"></i> Kembali ke Halaman Login</button>
             </div>
 
             <p style="text-align: center; font-size: 13px;">Hak Cipta &copy; <?= date('Y'); ?> .<?= $row1['nama_app']; ?> by FA Team.</p>
@@ -96,46 +76,6 @@ session_start();
     </script>
     <!-- Toastr -->
     <script src="assets/dist/js/toastr.min.js"></script>
-    <!-- -->
-    <script>
-        toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": true,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        }
-    </script>
-    <!-- -->
-    <script>
-        function validateForm() {
-            if (document.forms["formLogin"]["funame"].value == "") {
-                toastr.error("Nama Lengkap harus diisi !!");
-                document.forms["formLogin"]["funame"].focus()
-                return false;
-            }
-            if (document.forms["formLogin"]["uname"].value == "") {
-                toastr.error("Nama Pengguna harus diisi !!");
-                document.forms["formLogin"]["uname"].focus();
-                return false;
-            }
-            if (document.forms["formLogin"]["passw"].value == "") {
-                toastr.error("Kata Sandi harus diisi !!");
-                document.forms["formLogin"]["passw"].focus();
-                return false;
-            }
-        }
-    </script>
 </body>
 
 </html>
